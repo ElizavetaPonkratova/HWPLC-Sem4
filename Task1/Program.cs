1e4e98e9c6a589dc15c5907f0dfda18e660fa576
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-12 22:12:37 +0700

    HWSem4-1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не использовать метод Math.Pow
+Например: 3, 5 -> 243 (3⁵) 2, 4 -> 16*/
+
+Console.WriteLine("Enter number A:");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number B:");
+int B = int.Parse(Console.ReadLine());
+int num = 1;
+
+for (int i = 1; i <= B; i++)
+{
+    num = num * A;
+}
+
+Console.WriteLine(num);
