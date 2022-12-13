7c3c7ff33c6f461f982f53e69d805d54640ce083
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-13 21:01:40 +0700

    HWSem4-2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Например: 452 -> 11 82 -> 10 9012 -> 12*/
+
+Console.WriteLine("Enter number:");
+int a = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (a > 0)
+{
+    int num = a % 10;
+    a = a / 10;
+    sum = sum + num;
+}
+Console.WriteLine(sum);
