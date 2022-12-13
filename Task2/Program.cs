e39a66564deb7bc7777db5a7b6da94fbeaf61fba
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-13 21:14:51 +0700

    HWSem4-2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,13 +2,12 @@
 Например: 452 -> 11 82 -> 10 9012 -> 12*/
 
 Console.WriteLine("Enter number:");
-int a = int.Parse(Console.ReadLine());
+int num = int.Parse(Console.ReadLine());
 int sum = 0;
 
-while (a > 0)
+while (num > 0)
 {
-    int num = a % 10;
-    a = a / 10;
-    sum = sum + num;
+    sum = sum + num % 10;
+    num /= 10;
 }
 Console.WriteLine(sum);
