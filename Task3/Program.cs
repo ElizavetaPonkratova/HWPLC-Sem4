bfc42db8ec261f59a8dce74b17c87f20cf4e1b05
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-15 11:49:11 +0700

    HWSem4-3

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Например: 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 6, 1, 33 -> [6, 1, 33]*/
+
+Console.Write("Enter the number of array elements:");
+int elementsCount = Convert.ToInt32(Console.ReadLine());
+int[] myArray = new int[elementsCount];
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"Enter the array elements {i}:");
+    myArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Array output:");
+Console.Write("[");
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write(myArray[i] + ",");
+}
+Console.Write("]");
